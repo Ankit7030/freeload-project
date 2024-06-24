e9525ca5547fd268d1441a08c356b470fef884a8
Ankit7030 <ankit7030@gmail.com>
2024-06-24 21:39:23 +0530

    Update untitled25.py

--- untitled25.py
+++ untitled25.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import sys
 
-# Read input and output file paths from command-line arguments
-relo_file_path = sys.argv[1]
-fmer_file_path = sys.argv[2]
-output_file_path = sys.argv[3]
+relo_file = sys.argv[1]
+fmer_file = sys.argv[2]
+output_file = sys.argv[3]
 
 # Load the actual input dataframes
-relo_df = pd.read_excel(relo_file_path)
-fmer_df = pd.read_excel(fmer_file_path)
+relo_df = pd.read_csv(relo_file)
+fmer_df = pd.read_csv(fmer_file)
 
 # Ensure 'date' columns are in datetime format
 relo_df['date'] = pd.to_datetime(relo_df['date'])
@@ -121,7 +120,7 @@ for date in unique_dates:
     })
 
 # Write the outputs to an Excel file
-with pd.ExcelWriter(output_file_path) as writer:
+with pd.ExcelWriter(output_file) as writer:
     for date in unique_dates:
         date_str = pd.Timestamp(date).strftime('%Y-%m-%d')
         if date in cross_overs_dict:
